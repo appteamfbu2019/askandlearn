d63fb5688856c382a1c3b1a6853f790b12aba2aa
Esther Brown <estherb@fb.com>
2019-07-18 14:10:33 -0700

    Configuring assets and launch screen

---
+++ AskandLearn/AskandLearn/View Controller/LinkedInWebViewController.h
@@ -0,0 +1,17 @@
+//
+//  LinkedInWebViewController.h
+//  AskandLearn
+//
+//  Created by estherb on 7/18/19.
+//  Copyright © 2019 estherb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LinkedInWebViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
