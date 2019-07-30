7fcd822f3a89ba55d7ac6a57276bf0c497ede036
Esther Brown <estherb@fb.com>
2019-07-30 14:57:29 -0700

    View Controllers changes

--- AskandLearn/AskandLearn/View Controller/ChatViewController.h
+++ AskandLearn/AskandLearn/View Controller/ChatViewController.h
@@ -9,8 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface ChatViewController : UIViewController
-- (IBAction)didTapSend:(id)sender;
-- (IBAction)didTapBack:(id)sender;
 
 @property (weak, nonatomic) IBOutlet UITextField *messageTextField;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
